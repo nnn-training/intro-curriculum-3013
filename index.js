481a12f9939b5353718e679d6c2108a390582be8
takeshi <tksh0828@gmail.com>
2022-11-15 14:26:24 +0900

    C:/Program Files/Git/deleteメソッドのreqを追加

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-'use strict';
-const http = require('http');
+"use strict";
+const http = require("http");
 const server = http
   .createServer((req, res) => {
     const now = new Date();
     console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
     res.writeHead(200, {
-      'Content-Type': 'text/plain; charset=utf-8'
+      "Content-Type": "text/plain; charset=utf-8",
     });
 
     switch (req.method) {
-      case 'GET':
+      case "GET":
         res.write(`GET ${req.url}`);
         break;
-      case 'POST':
+      case "POST":
         res.write(`POST ${req.url}`);
-        let rawData = '';
+        let rawData = "";
         req
-          .on('data', chunk => {
+          .on("data", (chunk) => {
             rawData += chunk;
           })
-          .on('end', () => {
+          .on("end", () => {
             console.info(`[${now}] Data posted: ${rawData}`);
           });
         break;
+      case "DELETE":
+        res.write(`DELETE ${req.url}`);
       default:
         break;
     }
     res.end();
   })
-  .on('error', e => {
+  .on("error", (e) => {
     console.error(`[${new Date()}] Server Error`, e);
   })
-  .on('clientError', e => {
+  .on("clientError", (e) => {
     console.error(`[${new Date()}] Client Error`, e);
   });
 const port = 8000;
