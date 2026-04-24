d7590cf83840064428e1cdeb0815697ca1e91909
Etsuya Matsutani <220541632+etsu74@users.noreply.github.com>
2026-04-24 14:30:43 +0900

    DELETE

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ const server = http
             console.info(`[${now}] Data posted: ${rawData}`);
           });
         break;
-      default:
+      case 'DELETE':
+        res.write(`DELETE ${req.url}`);
+        break;
+        default:
         break;
     }
     res.end();
